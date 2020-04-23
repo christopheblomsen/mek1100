778c0f43e1b97ec457e8c435e39333b149b4f056
christopheblomsen <christophe.blomsen@gmail.com>
2020-04-23 06:19:08 +0200

    e done

--- oblig/oppgave_c.py
+++ oblig/oppgave_c.py
@@ -69,4 +69,38 @@ plt.contourf(x, y, divergence)
 plt.colorbar()
 plt.title("Oppgave d)")
 plt.savefig("oppgave_d.png")
+plt.close()
+
+"""
+oppgave e
+Had to redraw a lot so first couple of lines are just that
+"""
+rectangle(rectangle1_values[0], rectangle1_values[1],
+          rectangle1_values[2], rectangle1_values[3])
+rectangle2_values = [35, 85, 70, 100]
+rectangle(rectangle2_values[0], rectangle2_values[1],
+          rectangle2_values[2], rectangle2_values[3])
+rectangle3_values = [35, 50, 70, 60]
+rectangle(rectangle3_values[0], rectangle3_values[1],
+          rectangle3_values[2], rectangle3_values[3])
+plt.plot(xit, yit, "k*")
+num_skip = 5
+plt.quiver(x[::num_skip, ::num_skip], y[::num_skip, ::num_skip],
+           u[::num_skip, ::num_skip], v[::num_skip, ::num_skip])
+plt.plot(xit, yit, "k*")
+"""
+New stuff
+"""
+
+dudy = np.gradient(u, axis=1)
+dvdx = np.gradient(v, axis=0)
+
+curl_v = dvdx - dudy
+
+curl_plot = plt.contourf(x, y, curl_v)
+plt.colorbar()
+
+plt.title("Oppgave e)")
+
+plt.savefig("oppgave_e.png")
 plt.show()
