61eefe11d72fa3a91319712077cb8580378c4593
christopheblomsen <christophe.blomsen@gmail.com>
2020-05-01 21:10:15 +0200

    f done

--- oblig/oppgave_f.py
+++ oblig/oppgave_f.py
@@ -78,7 +78,7 @@ def line_integral(x1, y1, x2, y2):
         side4 -= k*dy
 
     sumation = side1 + side2 + side3 + side4
-    return sumation
+    return sumation, side1, side2, side3, side4
 
 
 def surface_integral(x1, y1, x2, y2):
@@ -92,17 +92,42 @@ def surface_integral(x1, y1, x2, y2):
     return integral
 
 
-print(line_integral(34, 159, 69, 169), line_integral(34, 84, 69, 99),
-      line_integral(34, 49, 69, 59))
-print(surface_integral(34, 159, 69, 169), surface_integral(34, 84, 69, 99),
-      surface_integral(34, 49, 69, 59))
+line_integral_1, side1_1, side2_1, side3_1, side4_1 = line_integral(34, 159, 69, 169)
+line_integral_2, side1_2, side2_2, side3_2, side4_2 = line_integral(34, 84, 69, 99)
+line_integral_3, side1_3, side2_3, side3_3, side4_3 = line_integral(34, 49, 69, 59)
 
+integral_1 = surface_integral(34, 159, 69, 169)
+integral_2 = surface_integral(34, 84, 69, 99)
+integral_3 = surface_integral(34, 49, 69, 59)
+
+print(f"Sirkulasjonen til rektangel 1 ble {line_integral_1}")
+print(f"Sirkulasjonen til rektangel 2 ble {line_integral_2}")
+print(f"Sirkulasjonen til rektangel 3 ble {line_integral_3}")
+
+print(f"""Rektangel 1 har side verdier: Side 1={side1_1},
+      Side 2={side2_1}, Side 3={side3_1}, Side 4={side4_1}""")
+print(f"""Rektangel 2 har side verdier: Side 1={side1_2},
+      Side 2={side2_2}, Side 3={side3_2}, Side 4={side4_2}""")
+print(f"""Rektangel 3 har side verdier: Side 1={side1_3},
+      Side 2={side2_3}, Side 3={side3_3}, Side 4={side4_3}""")
+"""
+
+Sirkulasjonen til rektangel 1 ble 1796.013421486005
+Sirkulasjonen til rektangel 2 ble -60206.56400779366
+Sirkulasjonen til rektangel 3 ble -143.18708039114364
+Rektangel 1 har side verdier: Side 1=70100.52387861427,
+      Side 2=-100.99982042140701, Side 3=-68332.85609978675, Side 4=129.34546307988458
+Rektangel 2 har side verdier: Side 1=198.47559740489203,
+      Side 2=919.0821556116496, Side 3=-61243.46477849595, Side 4=-80.65698231424645
+Rektangel 3 har side verdier: Side 1=5133.347850903836,
+      Side 2=175.1650519009061, Side 3=-5410.039721925995, Side 4=-41.660261269891
+"""
 """
 g
 """
 
 
-def gaus(x1, y1, x2, y2):
+def gauss(x1, y1, x2, y2):
     side1 = 0
     side2 = 0
     side3 = 0
